0edeaa90d28f83261eecca8f5693557e46d62f5b
Robert King <robert.o.king@hotmail.co.uk>
2020-04-08 01:05:52 +0200

    Create conversations model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_204256) do
+ActiveRecord::Schema.define(version: 2020_04_07_224601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_conversations_on_post_id"
+    t.index ["user_id"], name: "index_conversations_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id"
@@ -47,5 +56,7 @@ ActiveRecord::Schema.define(version: 2020_03_30_204256) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "conversations", "posts"
+  add_foreign_key "conversations", "users"
   add_foreign_key "posts", "users"
 end
