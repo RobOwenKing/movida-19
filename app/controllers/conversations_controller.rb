0af41ec83ef5c33e8f2bac3292c6522f0f47c50d
Robert King <robert.o.king@hotmail.co.uk>
2020-04-08 19:02:28 +0200

    Generate messages and conversations controllers

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,10 @@
+class ConversationsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'messages/new'
+  get 'messages/create'
+  get 'conversations/index'
+  get 'conversations/show'
+  get 'conversations/create'
   devise_for :users
   root to: 'pages#home'
 
